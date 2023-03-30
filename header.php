f5a02894931ac7f7353aa705cb081af77dded21c
Evgenii Kovalenko <kov.evgenii5@gmail.com>
2023-03-30 19:42:09 +0200

    header begin

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head itemscope itemtype="http://schema.org/WPHeader">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />    
+        
+    </head>
+    <body>
+        <header>      
+            <div class="header_container">
+                <div class="header_wrapper container">
+                    <div class="logo_img"><?php the_custom_logo() ?></div>
+                    <div class="header_menu"></div>
+
+                         
+                </div>                
+            </div>                    
+        </header>
+<main>        
